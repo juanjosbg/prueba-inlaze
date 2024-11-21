129a26c5a2748c8099e8f283ee688c8ca7e496aa
juanjosbg <juanjoseborrero95@gmail.com>
2024-11-21 14:00:24 -0500

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    reactRoot: true,
+    nextScriptWorkers: true,
+    turbopack: false,
+  }
 };
 
 export default nextConfig;
+
+
